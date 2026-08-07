85b1bacb3fa8debd8c0f6c92f9ab9586de20323f
Juhen Ahmed <nezmoanna@gmail.com>
2026-08-07 18:43:41 +0600

    Longest Word Finder

---
+++ Longest Word Finder/lngWordFinder.js
@@ -0,0 +1,19 @@
+function findLongestWordLength(sentence) {
+    let longestWordLength = 0;
+
+    for (let longestWord of sentence.split(" ")) {
+        let wordLength = longestWord.length;
+
+        if (wordLength > longestWordLength) {
+            longestWordLength = wordLength;
+        }
+    }
+
+    return longestWordLength;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("May the force be with you"));
+console.log(findLongestWordLength("Google do a barrel roll"));
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow"));
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
